5a5116f61f17e79d8814cf857e5ef674be34759a
Romstar744 <romstar742@gmail.com>
2024-11-11 20:09:59 +0300

    need to fix UnitTests

--- DataGridStarostin.Standart.ApplicantManager.Tests/ApplicantManagerTest.cs
+++ DataGridStarostin.Standart.ApplicantManager.Tests/ApplicantManagerTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using DataGridStarostin.Standart.Contracts;
 using DataGridStarostin.Standart.Contracts.Models;
@@ -18,14 +19,16 @@ namespace DataGridStarostin.Standart.ApplicantManager.Tests
         private readonly Mock<IApplicantStorage> applicantStorageMock;
         private readonly Mock<ILogger> loggerMock;
 
+        private readonly Task<IReadOnlyCollection<Applicant>> filledDefaultApplicantList;
+
         /// <summary>
         /// Инициализирует новый экземпляр класса <see cref="ApplicantManagerTest"/>.
         /// </summary>
         public ApplicantManagerTest()
         {
             applicantStorageMock = new Mock<IApplicantStorage>();
-            loggerMock = new Mock<ILogger>();
 
+            loggerMock = new Mock<ILogger>();
             loggerMock.Setup(x => x.Log(LogLevel.Information,
                 It.IsAny<EventId>(),
                 It.IsAny<It.IsAnyType>(),
@@ -66,6 +69,98 @@ namespace DataGridStarostin.Standart.ApplicantManager.Tests
             applicantStorageMock.Verify(x => x.AddAsync(It.Is<Applicant>(y => y.Id == model.Id)),
                 Times.Once);
             applicantStorageMock.VerifyNoOtherCalls();
+        }
+
+        /// <summary>
+        /// Тест: Метод <see cref="ApplicantManager.DeleteAsync"/>
+        /// </summary>
+        [Fact]
+        public async Task DeleteShouldWork()
+        {
+            // Arrange
+            var model = new Applicant
+            {
+                Id = Guid.NewGuid(),
+                Name = $"Name{Guid.NewGuid():N}",
+                Gender = Gender.Male,
+                Birthday = DateTime.Now,
+                Education = Education.FTPT,
+                Math = 52,
+                Russian = 52,
+                ComputerScience = 52,
+            };
+            applicantStorageMock.Setup(x => x.DeleteAsync(It.IsAny<Guid>()))
+                .ReturnsAsync(true);
+
+            // Act
+            var result = await applicantManager.DeleteAsync(model.Id);
+
+            // Asset
+            result.Should()
+                .Be(true);
+
+            applicantStorageMock.Verify(x => x.DeleteAsync(It.Is<Guid>(y => y == model.Id)),
+                Times.Once);
+            applicantStorageMock.VerifyNoOtherCalls();
+
+            loggerMock.Verify(x => x.Log(LogLevel.Information, It.IsAny<EventId>(), It.IsAny<It.IsAnyType>(), null,
+                It.IsAny<Func<It.IsAnyType, Exception, string>>()),
+                Times.Once);
+            loggerMock.VerifyNoOtherCalls();
+        }
+
+        /// <summary>
+        /// Изменение данных в хранилище
+        /// </summary>
+        [Fact]
+        public async Task EditAsyncShouldWork()
+        {
+            // Arrange
+            var model = new Applicant
+            {
+                Id = Guid.NewGuid(),
+                Name = $"Name{Guid.NewGuid():N}",
+                Gender = Gender.Male,
+                Birthday = DateTime.Now,
+                Education = Education.FTPT,
+                Math = 52,
+                Russian = 52,
+                ComputerScience = 52,
+            };
+            applicantStorageMock.Setup(x => x.EditAsync(It.IsAny<Applicant>()));
+
+            // Act
+            await applicantManager.EditAsync(model);
+
+            applicantStorageMock.Verify(x => x.EditAsync(It.Is<Applicant>(y => y.Id == model.Id)),
+                 Times.Once);
+            applicantStorageMock.VerifyNoOtherCalls();
+
+            loggerMock.Verify(x => x.Log(LogLevel.Information, It.IsAny<EventId>(), It.IsAny<It.IsAnyType>(), null,
+                It.IsAny<Func<It.IsAnyType, Exception, string>>()),
+                Times.Once);
+            loggerMock.VerifyNoOtherCalls();
+        }
+
+        /// <summary>
+        /// Получение данные из хранилища, если их нет
+        /// </summary>
+        [Fact]
+        public async Task GetAllAsyncShouldReturnEmpty()
+        {
+            // Arrange
+            applicantStorageMock.Setup(x => x.GetAllAsync());
+
+            // Act
+            var result = await applicantManager.GetAllAsync();
+
+            // Assert
+            result.Should().BeNull();
+
+            applicantStorageMock.Verify(x => x.GetAllAsync(), Times.Once);
+            applicantStorageMock.VerifyNoOtherCalls();
+
+            loggerMock.VerifyNoOtherCalls();
         }
     }
 }
